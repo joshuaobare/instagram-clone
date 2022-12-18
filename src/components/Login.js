17ff3a0ec9d64ef288627563272b0280101c28e4
joshuaobare <jobare.obare@gmail.com>
2022-12-18 22:46:59 +0300

    Login component updated, css updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,49 @@
 import LoginImg from "../images/Login.png"
+import FBicon from "../images/icons/FBicon.png"
+import Playstore from "../images/icons/Playstore.png"
+import Appstore from "../images/icons/Appstore.png"
+import IGlogo from "../images/icons/IGlogo.png"
+import { ReactComponent as LogoSvg } from "../images/icons/Logo.svg"
 
 export default function Login() {
     return (
         <div className="Login">
-            <div>
-                <img src={LoginImg} alt="Phones on login page" />
-                <div>
+            <div className="main">
+                <img src={LoginImg} alt="Phones on login page" className="login-main-img" />
+                <div className="form-section">
                     
+                    <form action="">
+                        {/*<LogoSvg /> */}
+                        <img src={IGlogo} alt="" />                       
+                        <div className="form-inputs">
+                            <input type="text" placeholder="Phone number, username, or email"/>
+                            <input type="password" placeholder="Password"/>                        
+                            <button className="form-button">Log in</button>
+                        </div>
+                        <div className="form-mid">
+                            <div className="form-mid-border"><hr /></div>
+                            <div className="form-mid-mid">OR</div>
+                            <div className="form-mid-border"><hr /></div>
+                        </div>
+                        <div className="form-bottom">
+                            <div className="FB-login">
+                                <span><img src={FBicon} alt="Facebook icon" className="FBicon" /></span> 
+                                Log in with Facebook
+                            </div>
+                            <div className="forgot-password">Forgot password?</div>
+                        </div>
+                                                
+                    </form>
+                    <div className="signup-prompt">
+                        Don't have an account? Sign Up
+                    </div>
+                    <div className="form-section-bottom">
+                        <div>Get the app.</div>
+                        <div className="app-stores">
+                            <img src={Appstore} alt="App store icon" />
+                            <img src={Playstore} alt="Playstore icon" />
+                        </div>
+                    </div>
                 </div>
             </div>
             <footer>
