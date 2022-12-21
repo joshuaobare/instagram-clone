f06c5a9248235d7cd93fa4170bb6538389c2c9f4
joshuaobare <jobare.obare@gmail.com>
2022-12-21 12:38:55 +0300

    moved firebase-config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import Navbar from "./components/Navbar";
 import Login from "./components/Login"
+import { getFirestore , collection, addDoc, getDocs } from "firebase/firestore"
+import { app } from "./firebase-config"
+
+
+
 function App() {
+
+  async function fetcher(){
+    const ref = await getDocs(collection(getFirestore(app), "profiles"))
+    ref.forEach((doc) => console.log(doc))
+  }
+
+  fetcher()
+
+
+
+
   return (
     <div className="App">
       <Login />
