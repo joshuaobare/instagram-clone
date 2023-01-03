655f4dc209411513bc680bf600e145bac7102061
joshuaobare <jobare.obare@gmail.com>
2023-01-03 14:13:09 +0300

    added Dialogs to Create component

--- src/components/Create.js
+++ src/components/Create.js
@@ -3,22 +3,29 @@ import {ReactComponent as UploadSvg} from "../images/icons/UploadImage.svg"
 import "../Create.css"
 
 export default function Create() {
-    return (
-        <div className="Create">            
-            <form action="">
-                <label className="image-input" htmlFor="image-input">
-                    <div>Upload Picture</div>
-                    <UploadSvg />                    
-                    <input type="file"
-                            id="image-input"
-                            accept="image/jpeg, image/png"
-                            style={{display:"none"}} />
-                </label>
-                
-                <label htmlFor="caption">Caption</label>
-                <textarea name="caption" id="caption" cols="30" rows="10"></textarea>
-                <button>Submit</button>
-            </form>
-        </div>
+    return (        
+            <Dialog>
+                <DialogTitle>Create a Post</DialogTitle>
+                <DialogContent>
+                    <div className="Create">
+                        <form action="">
+                            <label className="image-input" htmlFor="image-input">
+                                <div>Upload Picture</div>
+                                <UploadSvg />                    
+                                <input type="file"
+                                        id="image-input"
+                                        accept="image/jpeg, image/png"
+                                        style={{display:"none"}} />
+                            </label>
+                            
+                            <label htmlFor="caption">Caption</label>
+                            <textarea name="caption" id="caption" cols="30" rows="10"></textarea>
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                </DialogContent>                
+            </Dialog>            
+            
+        
     )
 }
